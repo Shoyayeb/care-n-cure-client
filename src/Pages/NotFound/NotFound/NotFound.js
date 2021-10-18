6a07527dc227790c8988f441bf9675e368ad629a
Shoyayeb <sa.fine@Outlook.com>
2021-10-19 01:08:16 +0600

    404 page updated

--- src/Pages/NotFound/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound/NotFound.js
@@ -22,30 +22,9 @@ const NotFound = () => {
                         <p className="text-base font-normal text-gray-600 dark:text-gray-300">
                             The page you ara looking for was not found.
                         </p>
-                        <p className="text-base font-normal text-gray-600 dark:text-gray-300">
-                            You may return to
-                            <Link to="/home" className="ml-2 text-blue-600 hover:underline dark:text-blue-500">home page</Link> or try
-                            using the search form.
-                        </p>
+
                     </div>
                 </div>
-
-                <form action="#" method="POST">
-                    <div className="flex items-center justify-center">
-                        <input
-                            type="text"
-                            name="search"
-                            placeholder="What are you looking for?"
-                            className="w-full px-4 py-2 rounded-l-md focus:outline-none focus:ring focus:ring-blue-400 dark:bg-darker dark:focus:ring-blue-800"
-                        />
-                        <button
-                            className="p-2 text-white rounded-r-md bg-blue-600 dark:bg-blue-700 hover:bg-blue-700 dark:hover:bg-blue-800 focus:outline-none focus:ring focus:ring-primary-light dark:focus:ring-primary-darker"
-                        >
-                            <span className="sr-only">Search</span>
-                            <FaSearch className="w-6 h-6"></FaSearch>
-                        </button>
-                    </div>
-                </form>
             </div>
         </main>
     );
